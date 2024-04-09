3572a11ea7f21fd64fb780323f548a20ce9e7aa1
IshratMishu <ishratjahan0425@gmail.com>
2024-04-09 19:59:04 +0600

    loading spinner added in estates section and made responsive section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     const [showAll, setShowAll] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    const handleSeeAll = ()=>{
+    const handleSeeAll = () => {
         setLoading(true);
         setTimeout(() => {
             setLoading(false);
@@ -25,19 +25,18 @@ const Home = () => {
         <div className="mt-24 font-poppins">
             <Banner></Banner>
             <div className="mt-32">
-            <div className="flex justify-between mt-32">
-            <h1 className="text-4xl font-semibold">Our Newest Properties</h1>
-           { !showAll && (<button onClick={handleSeeAll} className="flex items-center font-medium animate__animated animate__headShake animate__infinite">See All <IoIosArrowForward className=" font-bold text-accent"/></button>)}
+                <div className="flex justify-between mt-32">
+                    <h1 className="text-4xl font-semibold">Our Newest Properties</h1>
+                    {!showAll && (<button onClick={handleSeeAll} className="flex items-center font-medium animate__animated animate__headShake animate__infinite">See All <IoIosArrowForward className=" font-bold text-accent" /></button>)}
+                </div>
+                <PiDotsThreeOutlineFill className="text-3xl text-gray-300" />
             </div>
-            <PiDotsThreeOutlineFill className="text-3xl text-gray-300"/>
-        </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 mt-10 ">
-                { loading ? (
-                    <div className="text-center">
-                        <span className="loading loading-spinner loading-lg"></span>
-                    </div>)
-                 :
-                   showAll ? luxuryCards.map(estate => <EstatesData key={estate.id} estate={estate}></EstatesData>) :  luxuryCards.slice(0,6).map(estate => <EstatesData key={estate.id} estate={estate}></EstatesData>)
+                {loading ? (
+                    <span className="loading loading-spinner loading-lg ml-40 lg:ml-[550px] md:ml-96"></span>
+                )
+                    :
+                    showAll ? luxuryCards.map(estate => <EstatesData key={estate.id} estate={estate}></EstatesData>) : luxuryCards.slice(0, 6).map(estate => <EstatesData key={estate.id} estate={estate}></EstatesData>)
                 }
             </div>
         </div>
--- src/Pages/EstatesData.jsx
+++ src/Pages/EstatesData.jsx
@@ -10,39 +10,43 @@ const EstatesData = ({ estate }) => {
   return (
 
     <div className="card glass">
-      <figure className="w-auto relative"><img src={image} alt="car!"/>
-      <div className="badge badge-secondary badge-lg p-4 absolute right-5 top-14">{status}</div>
-      </figure>
-      <div className="card-body space-y-3">
-        <h2 className="card-title">{estate_title}</h2>
+    <figure className="w-auto relative"><img src={image} alt="car!"/>
+    <div className="badge badge-secondary badge-lg p-4 absolute right-5 top-14">{status}</div>
+    </figure>
+    <div className="card-body space-y-3">
+      <h2 className="card-title">{estate_title}</h2>
+    
+      <div className="flex flex-col flex-grow">
         <small className="flex items-center gap-1"><MdOutlineLocationOn /> {location}</small>
-        <div className="overflow-x-auto">
-          <table className="table table-zebra">
-            <tbody>
-              <tr>
-                <td>Segment</td>
-                <td>{segment_name}</td>
-              </tr>
-              <tr>
-                <td>Price</td>
-                <td>{price}</td>
-              </tr>
-              <tr>
-                <td>Built Year</td>
-                <td>{year_built}</td>
-              </tr>
-              <tr>
-                <td>Area</td>
-                <td>{area}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="card-actions justify-end">
-          <Link to={`/luxuryEstates/${id}`}><button>View Property</button></Link>
-        </div>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          <tbody>
+            <tr>
+              <td>Segment</td>
+              <td>{segment_name}</td>
+            </tr>
+            <tr>
+              <td>Price</td>
+              <td>{price}</td>
+            </tr>
+            <tr>
+              <td>Built Year</td>
+              <td>{year_built}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>{area}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="card-actions justify-end flex flex-col flex-grow">
+        <Link to={`/luxuryEstates/${id}`}><button className="btn btn-outline ml-20">View Property</button></Link>
       </div>
     </div>
+  </div>
 
   );
 };
